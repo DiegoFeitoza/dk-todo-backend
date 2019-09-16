162ff977fdce8c3a4c8eb9ff1d652684d9bdfa86
Feitoza, Diego <diego.feitoza@accenture.com>
2019-09-16 13:22:43 -0300

    Att heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,3 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb+srv://deploy:todoUpload@cluster0-4eb0q.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true}, (err, res) => {
-    if(err){
-        throw err
-    }else{
-        console.log('Exec')
-    }
-})
+module.exports = mongoose.connect('mongodb+srv://deploy:todoUpload@cluster0-4eb0q.mongodb.net/test?retryWrites=true&w=majority')
