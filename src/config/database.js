ea7382e9965cfa144d0c27e8217ff4f774b8d213
Diego Feitoza <diegofeitoza@yahoo.com.br>
2019-09-17 22:19:20 -0300

    att heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,4 @@
+require('mongodb')
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb+srv://deploy:todoUpload@cluster0-4eb0q.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true },(err,res)=>{
