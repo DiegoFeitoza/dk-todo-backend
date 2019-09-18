263670e3dec7d626dcbf03e9472c79f67663423c
Diego Feitoza <diegofeitoza@yahoo.com.br>
2019-09-17 23:02:11 -0300

    Att heroku em nome do senhor

--- src/config/database.js
+++ src/config/database.js
@@ -8,11 +8,3 @@
 //         console.log('connect mongodb...')
 //     }
 // });
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://deploy:todoUpload@cluster0-4eb0q.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
--- src/config/server.js
+++ src/config/server.js
@@ -1,4 +1,4 @@
-const port = 5005 // Add porta
+// const port = 5005 // Add porta
 
 //Add body parser => Passar todas as instancias de backend para o servidor
 const bodyParser = require('body-parser')
@@ -6,13 +6,66 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const allowCors = require('./cors')
+let collection
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://deploy:todoUpload@cluster0-4eb0q.mongodb.net/crud?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  collection = client.db("crud").collection("usuarios");
+  // perform actions on the collection object
+//   client.close();
+});
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
 
-server.listen(process.env.PORT, function(){
-    console.log(`Rodando BACKEND na porta `)
+server.get('/', function(req, res){
+    res.send('Bem-vindo a API CRUD')
+})
+
+server.get('/lista', (req,res) => {
+    collection.find({}).toArray(function(err, docs) {
+        res.send(docs);
+    });
+})
+
+//Criando Rotas
+server.route('/edit/:id')
+    .get((req,res) => {
+        //Pesquisar específico
+        collection.find({"_id" : ObjectId(req.params.id)}).toArray(function(err, docs) {
+            res.send(docs)
+        });
+    })
+    .put((req,res) => {
+        //Alterar
+        collection.updateOne({ _id : ObjectId(req.params.id) },
+        { $set: { name : req.body.name,
+                    surname: req.body.surname        
+        } }, function(err, result) {
+            res.send(`Usuário ${req.body.name} editado com sucesso!`)
+        });
+    })
+    .delete((req,res) => {
+        //Deletar
+        collection.deleteOne({"_id" : ObjectId(req.params.id)},function(err, resp){
+            res.send(`Usuário ${req.params.id} apagado com sucesso!`)
+        })
+    })
+
+server.post('/show',function(req, res){
+    collection.insertOne(req.body, (err, result) => {
+        //Gravar
+        if(err) return console.log(err);
+        res.send(`Usuário ${req.body.name} salvo com sucesso!`)
+    })
+})
+
+let port = (process.env.PORT) ?process.env.PORT : 5005
+
+server.listen(port, function(){
+    console.log(`Rodando BACKEND na porta ${port}`)
 })
 
 module.exports = server
